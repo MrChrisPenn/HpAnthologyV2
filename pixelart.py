593571c3dbf120aded68508c688f188b99548346
ncscomputing <cnpenn@gmail.com>
2017-07-19 10:04:53 +0100

    Add files via upload

---
+++ pixelart.py
@@ -0,0 +1,75 @@
+from sense_emu import SenseHat
+import time
+import random
+
+s = SenseHat()
+s.low_light = False
+
+green = (0, 255, 0)
+yellow = (255, 255, 0)
+blue = (0, 0, 255)
+red = (255, 0, 0)
+white = (255,255,255)
+nothing = (0,0,0)
+pink = (255,105, 180)
+
+ColourList = [green,yellow,blue,red,white,pink]
+
+
+def raspi_logo():
+    G = green
+    R = red
+    O = nothing
+    logo = [
+    O, G, G, O, O, G, G, O, 
+    O, O, G, G, G, G, O, O,
+    O, O, R, R, R, R, O, O, 
+    O, R, R, R, R, R, R, O,
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    O, R, R, R, R, R, R, O,
+    O, O, R, R, R, R, O, O,
+    ]
+    return logo
+
+def raspi_logo_random():
+    G = random.choice(ColourList)
+    R = random.choice(ColourList)
+    O = random.choice(ColourList)
+    logo = [
+    O, G, G, O, O, G, G, O, 
+    O, O, G, G, G, G, O, O,
+    O, O, R, R, R, R, O, O, 
+    O, R, R, R, R, R, R, O,
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    O, R, R, R, R, R, R, O,
+    O, O, R, R, R, R, O, O,
+    ]
+    return logo
+
+
+
+
+def heart():
+    P = pink
+    O = nothing
+    logo = [
+    O, O, O, O, O, O, O, O,
+    O, P, P, O, P, P, O, O,
+    P, P, P, P, P, P, P, O,
+    P, P, P, P, P, P, P, O,
+    O, P, P, P, P, P, O, O,
+    O, O, P, P, P, O, O, O,
+    O, O, O, P, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    ]
+    return logo
+
+
+while True: 
+    s.set_pixels(raspi_logo())#images[count % len(images)]())
+    time.sleep(0.75)
+    s.set_pixels(raspi_logo_random())
+    time.sleep(0.75)
+    #count += 1
